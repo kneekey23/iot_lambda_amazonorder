87119a300afbc9d62eb242d9e2cbc82f66ee22d6
Klein <nickik@amazon.com>
2018-08-13 21:37:00 -0700

    initial commit

--- IOT_Lambda/Function.cs
+++ IOT_Lambda/Function.cs
@@ -2,8 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using Amazon.Lambda.Core;
+using System.Text;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -12,16 +19,150 @@ namespace IOT_Lambda
 {
     public class Function
     {
-        
         /// <summary>
         /// A simple function that takes a string and does a ToUpper
         /// </summary>
         /// <param name="input"></param>
         /// <param name="context"></param>
         /// <returns></returns>
-        public string FunctionHandler(string input, ILambdaContext context)
+        private static AmazonSimpleNotificationServiceClient snsClient = new AmazonSimpleNotificationServiceClient(Amazon.RegionEndpoint.USWest2);
+        private static AmazonDynamoDBClient dynamoClient = new AmazonDynamoDBClient(Amazon.RegionEndpoint.USWest2);
+        private static DynamoDBContext dynamoContext = new DynamoDBContext(dynamoClient);
+
+
+        public async Task FunctionHandler(ButtonInput input, ILambdaContext context)
         {
-            return input?.ToUpper();
+            //place amazon order
+     
+            Users user = await GetUser(input.serialNumber);
+            context.Logger.LogLine("Order Placed:" + user.orderPlaced.ToString());
+            if (!user.orderPlaced)
+            {
+                await ZincRequest(user, context);
+            }
         }
+
+        public async Task ZincRequest(Users user, ILambdaContext context)
+        {
+            context.Logger.LogLine("inside Zinc function");
+            Order newOrder = new Order(user.firstName, user.lastName, user.address, user.city, user.state, user.zip, user.phone, user.productId);
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://api.zinc.io");
+                client.DefaultRequestHeaders.Accept.Clear();
+                var byteArray = Encoding.ASCII.GetBytes("22F7462B4C97B3EE889DAF33:");
+
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var content = new StringContent(newOrder.ToJson().ToString(), Encoding.UTF8, "application/json");
+                var result = await client.PostAsync("/v1/orders", content);
+                result.EnsureSuccessStatusCode();
+                string resultContent = await result.Content.ReadAsStringAsync();
+            
+                //if order was placed on the queue send text message
+                if (result.IsSuccessStatusCode)
+                {
+                    Dictionary<string, MessageAttributeValue> smsAttributes = new Dictionary<string, MessageAttributeValue>();
+
+                    smsAttributes.Add("AWS.SNS.SMS.SMSType", new MessageAttributeValue()
+                    {
+                        StringValue = "Transactional",
+                        DataType = "String"
+
+                    });
+
+                    //publish requestid to topic
+                    await PublishRequestIdToTopic(resultContent, smsAttributes, context);
+                    //send message to user
+                    await SendSMSMessage("Your order has been entered into the queue", user.phone, smsAttributes, context);
+
+                    //save order request id to User
+                    await UpdateUser(user.id, resultContent, context);
+
+                }
+
+
+            }
+        }
+
+        public async Task UpdateUser(string id, string requestId, ILambdaContext context)
+        {
+            Users user = await dynamoContext.LoadAsync<Users>(id);
+
+            user.orderPlaced = true;
+            user.requestId = requestId;
+
+            await dynamoContext.SaveAsync(user);
+   
+        }
+
+        public async Task SendSMSMessage(string message,
+        string phoneNumber, Dictionary<string, MessageAttributeValue> smsAttributes, ILambdaContext context)
+        {
+            PublishRequest request = new PublishRequest();
+            request.Message = message;
+            request.PhoneNumber = phoneNumber;
+            request.MessageAttributes = smsAttributes;
+
+            await snsClient.PublishAsync(request);
+
+        }
+
+        public async Task PublishRequestIdToTopic(string requestId, Dictionary<string, MessageAttributeValue> smsAttributes, ILambdaContext context)
+        {
+            PublishRequest request = new PublishRequest();
+            request.TopicArn = "arn:aws:sns:us-west-2:805580953652:UserOrder";
+            request.MessageAttributes = smsAttributes;
+            request.Message = requestId;
+            await snsClient.PublishAsync(request);
+        }
+        public async Task<Users> GetUser(string id)
+        {
+            Users user = await dynamoContext.LoadAsync<Users>(id);
+            return user;
+        }
+
+    }
+
+    public partial class Users
+    {
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public bool orderPlaced { get; set; }
+        public string requestId { get; set; }
+        public string address { get; set; }
+
+        public string city { get; set; }
+
+        public string state { get; set; }
+
+        public string zip { get; set; }
+
+        public string phone { get; set; }
+
+        public string productId { get; set; }
+    }
+
+
+    [DynamoDBTable("Users")]
+    public partial class Users
+    {
+        [DynamoDBHashKey]
+        public string id { get; set;}
+    }
+
+    public class ButtonInput
+    {
+        public string serialNumber { get; set; }
+
+        public string batteryVoltage { get; set; }
+
+        public string clickType { get; set; }
     }
 }
---
+++ IOT_Lambda/Order.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace IOT_Lambda
+{
+    public partial class Order
+    {
+        public Order(string firstName, string lastName, string address, string city, string state, string zip, string phone, string productId)
+        {
+            Retailer = "amazon";
+            Products = new List<Product>();
+            Product product = new Product
+            {
+                Quantity = 1,
+                ProductId = productId
+            };
+            Products.Add(product);
+
+            //shipping
+            ShippingAddress = new IngAddress();
+            ShippingAddress.PhoneNumber = phone;
+            ShippingAddress.State = state;
+            ShippingAddress.FirstName = firstName;
+            ShippingAddress.LastName = lastName;
+            ShippingAddress.City = city;
+            ShippingAddress.AddressLine1 = address;
+            ShippingAddress.ZipCode = zip;
+            ShippingAddress.Country = "US";
+            IsGift = false;
+            MaxPrice = 10000;
+            Shipping = new Shipping();
+            Shipping.MaxDays = 2;
+            Shipping.OrderBy = "price";
+
+            //extract payment method from json file
+            var path = "payment.json";
+            using (StreamReader r = new StreamReader(path: path))
+            {
+                string json = r.ReadToEnd();
+                PaymentMethod = JsonConvert.DeserializeObject<PaymentMethod>(json);
+    
+            }
+            //billing
+            BillingAddress = new IngAddress();
+            BillingAddress.AddressLine1 = "2030 8th Ave";
+            BillingAddress.AddressLine2 = "Unit 1805";
+            BillingAddress.City = "Seattle";
+            BillingAddress.State = "WA";
+            BillingAddress.Country = "US";
+            BillingAddress.FirstName = "Nicole";
+            BillingAddress.LastName = "Klein";
+            BillingAddress.PhoneNumber = "714-925-5830";
+            BillingAddress.ZipCode = "98121";
+
+
+            // get retailer creds from json file
+            using (StreamReader r = new StreamReader("amazonCreds.json"))
+            {
+                string json = r.ReadToEnd();
+                RetailerCredentials = JsonConvert.DeserializeObject<RetailerCredentials>(json);
+            }
+
+        }
+
+        [JsonProperty("retailer")]
+        public string Retailer { get; set; }
+
+        [JsonProperty("products")]
+        public List<Product> Products { get; set; }
+
+        [JsonProperty("max_price")]
+        public long MaxPrice { get; set; }
+
+        [JsonProperty("shipping_address")]
+        public IngAddress ShippingAddress { get; set; }
+
+        [JsonProperty("is_gift")]
+        public bool IsGift { get; set; }
+
+        [JsonProperty("gift_message")]
+        public string GiftMessage { get; set; }
+
+        [JsonProperty("shipping")]
+        public Shipping Shipping { get; set; }
+
+        [JsonProperty("payment_method")]
+        public PaymentMethod PaymentMethod { get; set; }
+
+        [JsonProperty("billing_address")]
+        public IngAddress BillingAddress { get; set; }
+
+        [JsonProperty("retailer_credentials")]
+        public RetailerCredentials RetailerCredentials { get; set; }
+
+        [JsonProperty("webhooks")]
+        public Webhooks Webhooks { get; set; }
+
+        [JsonProperty("client_notes")]
+        public ClientNotes ClientNotes { get; set; }
+    }
+
+    public partial class IngAddress
+    {
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+
+        [JsonProperty("address_line1")]
+        public string AddressLine1 { get; set; }
+
+        [JsonProperty("address_line2")]
+        public string AddressLine2 { get; set; }
+
+        [JsonProperty("zip_code")]
+        public string ZipCode { get; set; }
+
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        [JsonProperty("phone_number")]
+        public string PhoneNumber { get; set; }
+    }
+
+    public partial class ClientNotes
+    {
+        [JsonProperty("our_internal_order_id")]
+        public string OurInternalOrderId { get; set; }
+
+        [JsonProperty("any_other_field")]
+        public List<string> AnyOtherField { get; set; }
+    }
+
+    public partial class PaymentMethod
+    {
+        [JsonProperty("name_on_card")]
+        public string NameOnCard { get; set; }
+
+        [JsonProperty("number")]
+        public string Number { get; set; }
+
+        [JsonProperty("security_code")]
+        [JsonConverter(typeof(ParseStringConverter))]
+        public long SecurityCode { get; set; }
+
+        [JsonProperty("expiration_month")]
+        public long ExpirationMonth { get; set; }
+
+        [JsonProperty("expiration_year")]
+        public long ExpirationYear { get; set; }
+
+        [JsonProperty("use_gift")]
+        public bool UseGift { get; set; }
+    }
+
+    public partial class Product
+    {
+        [JsonProperty("product_id")]
+        public string ProductId { get; set; }
+
+        [JsonProperty("quantity")]
+        public long Quantity { get; set; }
+    }
+
+    public partial class RetailerCredentials
+    {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+
+        [JsonProperty("totp_2fa_key")]
+        public string Totp2FaKey { get; set; }
+    }
+
+    public partial class Shipping
+    {
+        [JsonProperty("order_by")]
+        public string OrderBy { get; set; }
+
+        [JsonProperty("max_days")]
+        public long MaxDays { get; set; }
+
+        [JsonProperty("max_price")]
+        public long MaxPrice { get; set; }
+    }
+
+    public partial class Webhooks
+    {
+        [JsonProperty("request_succeeded")]
+        public string RequestSucceeded { get; set; }
+
+        [JsonProperty("request_failed")]
+        public string RequestFailed { get; set; }
+
+        [JsonProperty("tracking_obtained")]
+        public string TrackingObtained { get; set; }
+    }
+
+    public partial class Order
+    {
+        public static Order FromJson(string json) => JsonConvert.DeserializeObject<Order>(json, IOT_Lambda.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this Order self) => JsonConvert.SerializeObject(self, IOT_Lambda.Converter.Settings);
+    }
+
+    internal static class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters = {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+
+    internal class ParseStringConverter : JsonConverter
+    {
+        public override bool CanConvert(Type t) => t == typeof(long) || t == typeof(long?);
+
+        public override object ReadJson(JsonReader reader, Type t, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null) return null;
+            var value = serializer.Deserialize<string>(reader);
+            long l;
+            if (Int64.TryParse(value, out l))
+            {
+                return l;
+            }
+            throw new Exception("Cannot unmarshal type long");
+        }
+
+        public override void WriteJson(JsonWriter writer, object untypedValue, JsonSerializer serializer)
+        {
+            if (untypedValue == null)
+            {
+                serializer.Serialize(writer, null);
+                return;
+            }
+            var value = (long)untypedValue;
+            serializer.Serialize(writer, value.ToString());
+            return;
+        }
+
+        public static readonly ParseStringConverter Singleton = new ParseStringConverter();
+    }
+}
